fb9c0894953886303b327dde345fba76cb67e76d
song285 <alen285@qq.com>
2023-03-17 17:21:28 +0800

    修改默认语言

--- src/i18n.ts
+++ src/i18n.ts
@@ -11,7 +11,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: 'zh-CN',
     ns: ['main', 'api', 'about', 'model'],
     defaultNS: 'main',
   });
